8a3e2969a54b5ce791d8c89604f308bf2ac22cfa
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-04 16:16:14 +0700

    Release v0.001

--- lib/Graphics/ColorNames/FamousLogo.pm
+++ lib/Graphics/ColorNames/FamousLogo.pm
@@ -11,40 +11,58 @@ use warnings;
 sub NamesRgbTable() {
     use integer;
     return {
-        google_blue   => "3369e8",
-        google_red    => "d50f25",
-        google_yellow => "eeb211",
-        google_green  => "009925",
 
-        nbc_yellow => "fcb711",
-        nbc_orange => "f37021",
-        nbc_red    => "cc004c",
-        nbc_purple => "6460aa",
-        nbc_blue   => "0089d0",
-        nbc_green  => "0db14b",
+        airbnb_red => "fd5c63",
+
+        alibaba_orange => "ff6a00",
+
+        amazon_orange => "ff9900",
+        amazon_blue   => "146eb4",
+
+        americanexpress_blue => "002663",
+        americanexpress_gray => "4d4f53",
+
+        android_green => "a4c639",
+
+        att_blue => "00a8e0",
+
+        baidu_red => "de0f17",
+        baidu_blue => "2529d8",
+
+        apple1980_blue   => "009ddc",
+        apple1980_green  => "61bb46",
+        apple1980_orange => "f5821f",
+        apple1980_purple => "963d97",
+        apple1980_red    => "e03a3e",
+        apple1980_yellow => "fdb827",
 
         ballpark_black  => "1a1716",
-        ballpark_red1   => "b2171d",
-        ballpark_white  => "ffffff",
         ballpark_blue   => "1d3f99",
+        ballpark_red1   => "b2171d",
         ballpark_red2   => "ec0f20",
+        ballpark_white  => "ffffff",
         ballpark_yellow => "f1d408",
+
+        google_blue   => "3369e8",
+        google_green  => "009925",
+        google_red    => "d50f25",
+        google_yellow => "eeb211",
+
+        nascar_blue   => "02549e",
+        nascar_purple => "88166d",
+        nascar_red    => "bc2021",
+        nascar_yellow => "edc240",
+
+        nbc_blue   => "0089d0",
+        nbc_green  => "0db14b",
+        nbc_orange => "f37021",
+        nbc_purple => "6460aa",
+        nbc_red    => "cc004c",
+        nbc_yellow => "fcb711",
 
         nfl_blue  => "003a73",
         nfl_red   => "da2127",
         nfl_white => "ffffff",
-
-        nascar_yellow => "edc240",
-        nascar_red    => "bc2021",
-        nascar_purple => "88166d",
-        nascar_blue   => "02549e",
-
-        apple1980_green  => "61bb46",
-        apple1980_yellow => "fdb827",
-        apple1980_orange => "f5821f",
-        apple1980_red    => "e03a3e",
-        apple1980_purple => "963d97",
-        apple1980_blue   => "009ddc",
     };
 }
 
@@ -67,3 +85,5 @@ sub NamesRgbTable() {
 =head1 SEE ALSO
 
 L<Graphics::ColorNames>
+
+L<https://digitalsynopsis.com/design/brand-colors/>
